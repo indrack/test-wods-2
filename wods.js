010c33dc0833cc38beec4bdeecfc3416a776405c
indrack <34111116+indrack@users.noreply.github.com>
2026-06-21 11:21:18 -0400

    Update weekly workout structure and details

--- wods.js
+++ wods.js
@@ -4,28 +4,27 @@ const wods = {
   domingo: [
     {
       titulo: "WARM-UP",
-      contenido: `*2 Rounds*
-10m High Knees
-10m Butt Kicks
-10m High Skips
-100m Moderate Run
-4 Up Downs`
+      contenido: `3-4 Rounds
+20 Single Unders
+10 Alternating Lunges
+10 Hollow Rocks
+5 Single Arm Dumbbell Clean and Jerks *(each)*`
     },
     {
-      titulo: "Custom Metcon (Tiempo)",
-      contenido: `*Arrowhead Stadium*
-For time:
-1 Mile Run
-*Every 1 min (starting at 0:00) complete:*
-4 Burpees
-Time cap: 17 mins`
+      titulo: "Custom Metcon",
+      contenido: `*Late Nights*
+*8 rounds*, each round for time, of:
+200m Run
+75 Double Unders
+100 ft Single Arm Dumbbell Overhead Carry, 32.5/22.5
+Rest 1:30 between each round.`
     },
     {
       titulo: "Accesorio",
-      contenido: `*3 rounds for quality of:*
-50 Kettlebell Side Bends,* RPE 7/10*
-Side Plank, 1:30
-Rest 1-1:30 min between rounds.`
+      contenido: `*4 rounds for quality of:*
+60 Single Leg Banded Hamstring Curls
+30 Banded Leg Curls
+Rest 1 min`
     }
   ],
 
@@ -34,24 +33,25 @@ Rest 1-1:30 min between rounds.`
     {
       titulo: "WARM-UP",
       contenido: `*2 Rounds*
-5 Roll and Reach
-3 Inchworms
-5 PVC Deadlift
-5 Hang Power Clean
-5 Strict Press
-2x50ft Shuttle Run`
+10 Banded Pass Throughs
+10 PVC Muscle Snatch
+10 Air Squats
+4 Hang Power Snatches (light)
+4 Hanging Knee Raises`
     },
     {
-      titulo: "Weightlifting (Clean and Jerk)",
-      contenido: `1x [ 2 Power Cleans + 1 Push Jerk ], *65% 1RM or RPE 6*
-*Every 1:30 for 9 mins.*`
+      titulo: "Weightlifting (Snatch)",
+      contenido: `1x [ 1 Snatch Deadlift + 1 Power Snatch + 1 Overhead Squat ]
+Start light and build each minute.
+*Every 1 min for 10 mins.*`
     },
     {
       titulo: "Custom Metcon (Tiempo)",
-      contenido: `*Gillette Stadium*
-*8 rounds*, each round for time, of:
-100 m Run
-5 Clean & Jerks, 60/42.5 ( touch and go)`
+      contenido: `*Catching Fireflies*
+*10 rounds* for time of:
+16 Air Squats
+8 Toes-to-bars
+4 Power Snatches, 60/42.5`
     }
   ],
 
@@ -59,36 +59,28 @@ Rest 1-1:30 min between rounds.`
   martes: [
     {
       titulo: "WARM-UP",
-      contenido: `2-3 Rounds
-10 Glute Bridges
-5 Tempo Air Squats (3 seconds down)
-6 Banded Good Mornings
-10 Deadbugs
-5 Tempo Goblet Squats (light)
-20-second Moderate Row`
+      contenido: `0:00–2:00 Easy Jump Rope
+-then-
+*3 Rounds*
+10 Banded Pass Throughs
+10 Banded Good Mornings
+10 Alternating V-Ups
+5 Up Downs + Lateral Jump over PVC (be careful)
+5 PVC Strict Press`
     },
     {
-      titulo: "Weightlifting (Back Squat)",
-      contenido: `3 Tempo Back Squats, *60–65%*
-3 sec descent
-*Every 2 mins for 10 mins.*`
+      titulo: "Custom Metcon (Reps)",
+      contenido: `*JUNE BURPEE CHALLENGE*
+Tabata Burpee Over Bar
+The Tabata interval is 45 secs of work followed
+by 15 secs of rest for 12 intervals.
+Tabata score is the total reps performed in all of the intervals.`
     },
     {
-      titulo: "Custom Metcon (Tiempo)",
-      contenido: `*BMO Field*
-*Teams of 3*
-For time:
-15 Cal Row (5 each athlete)
-15 Kettlebell Goblet Squats 32/24(5 each athlete)
-30 Cal Row (10 each athlete)
-30 Kettlebell Goblet Squats 32/24(10 each athlete)
-45 Cal Row (15 each athlete)
-45 Kettlebell Goblet Squats 32/24(15 each athlete)
-60 Cal Row (20 each athlete)
-60 Kettlebell Goblet Squats 32/24(20 each athlete)
-75 Cal Row (25 each athlete)
-75 Kettlebell Goblet Squats 32/24(25 each athlete)
-Time cap: 20 mins`
+      titulo: "Weightlifting (Press)",
+      contenido: `*5 Sets* Every 2:00 (Post Workout)
+3 Strict Press + 3 Push Press
+Start light and build each minute.`
     }
   ],
 
@@ -96,39 +88,33 @@ Time cap: 20 mins`
   miercoles: [
     {
       titulo: "WARM-UP",
-      contenido: `2-3 Rounds
-10 Banded Pass Throughs
-10 Banded Pull Aparts
-10-second Deadhang
-10 Alternating V-Ups
-20 Single Unders
-3 Pike Push-Ups
-10-Second Ring Support Hold
-5 Box Dips or Push Ups or 5-second Plank Hold`
-    },
-    {
-      titulo: "Gymnastics (Ring Dip)",
-      contenido: `*Level 1*
-As many reps as possible in 8 mins of:
-Incline Push-up
-*Level 2*
-As many reps as possible in 8 mins of:
-Box Dip
-*Level 3*
-As many reps in 8 mins as you can of:
-Strict Ring Dip`
+      contenido: `*2 Rounds*
+100m Moderate Run
+10 Roll and Reach
+10 Glute Bridges
+10 Deadbugs
+10 Banded Good Mornings
+4 Deadlifts (light)
+4 Push-Ups`
     },
     {
       titulo: "Custom Metcon (Tiempo)",
-      contenido: `*SoFi Stadium*
-For time:
-50 V-ups
--- then --
-*7 rounds* of:
-7 Strict Handstand Push-ups
-49 Double Unders
--- then --
-50 V-ups`
+      contenido: `*Summer Sunsets*
+*5 rounds*, each round for time, of:
+400 m Run
+8 Deadlifts, 142.5/92.5`
+    },
+    {
+      titulo: "Accesorio",
+      contenido: `*Accessory A*
+Strict Pull-ups 4x10
+Rest 1-1:30 min between sets.
+Directly into B...
+Accessory B
+Tabata Bent Over Plate Row, pick load
+The Tabata interval is 20 secs of work followed
+by 10 secs of rest for 8 intervals.
+Tabata score is the total reps performed in all of the intervals.`
     }
   ],
 
@@ -136,28 +122,26 @@ For time:
   jueves: [
     {
       titulo: "WARM-UP",
-      contenido: `*2 Rounds*
-10 Step Back Lunges
-10 Roll and Reach
-3 Inchworms
-10ft Dumbbell Farmer Carry Walking Lunge (light)
-3 Up Downs Over Dumbbells`
+      contenido: `2-3 Rounds
+10 Band Pull Aparts
+5 Hand Release Push Ups
+10 Box Step Ups
+10 Alternating V-Ups
+10 SIngle Arm Upright Rows *(each)*`
     },
     {
-      titulo: "Custom Metcon",
-      contenido: `*Ford Field*
-*5 rounds*, 3 mins each, for max reps of:
-50 Air Squats
-50 ft Dumbbell Walking Lunge, 22.5/15
-max reps in remaining time Burpee Over Dumbbells
-Rest 1 min`
+      titulo: "Weightlifting (Bench Press)",
+      contenido: `Bench Press 10-8-6-4-2
+Use the heaviest weight you can for each set.
+Rest as needed between sets.`
     },
     {
-      titulo: "Accesorio",
-      contenido: `*Tabata Hip Extension / Superman*
-The Tabata interval is 20 secs of work followed
-by 10 secs of rest for 8 intervals.
-Tabata score is the total reps performed in all of the intervals.`
+      titulo: "Custom Metcon (Tiempo)",
+      contenido: `*Ice Cold Lemonade*
+*2 rounds* for time of:
+50 V-ups
+50 Box Step-ups, 24/20 in
+50 Push-ups`
     }
   ],
 
@@ -166,24 +150,29 @@ Tabata score is the total reps performed in all of the intervals.`
     {
       titulo: "WARM-UP",
       contenido: `*2 Rounds*
-10 Glute Bridges
-10 Deadbugs
-5 Tempo Banded Good Mornings (3-second down, stand)
-3 Lunge Matrix *(each)*
-5 Empty Bar Deadlifts
-5 Hand Release Push Ups`
+10 Banded Pass Throughs
+10 Banded Pull Aparts
+10 Banded Strict Press
+5 Dynamic Squat Stretches
+5 Kip Swings`
     },
     {
-      titulo: "Weightlifting (Deadlift)",
-      contenido: `3 Pause Deadlifts, Mid-shin for 2-3 seconds *@ 60–65%*
-*Every 2 mins for 10 mins.*`
+      titulo: "Gymnastics (Pull-up)",
+      contenido: `Pull-Up Efficiency
+Teaching the pull-up, focusing on rhythm, efficiency, and sustainable gymnastics pacing under fatigue.
+5 Kip Swings + Knee Raise
+3 Kipping Pull-Ups
+3 Butterfly Pull-Ups (if appropriate)`
     },
     {
       titulo: "Custom Metcon (Tiempo)",
-      contenido: `*Lumen Field*
-*5 rounds* for time of:
-10 Deadlifts, 125/85
-20 Push-ups`
+      contenido: `*Cookout Season*
+*“TEAMS OF 3”*
+*“FOR TIME”*
+2100/1700m Row
+200 Thrusters *(20/15)*
+150 Pull-Ups
+Time cap: 20 mins`
     }
   ],
 
@@ -192,31 +181,35 @@ Tabata score is the total reps performed in all of the intervals.`
     {
       titulo: "WARM-UP",
       contenido: `*2 Rounds*
-1:00 Single Unders
-*3 Rounds*
-10-second Active Dead Hang
-5 Ring Rows
-6 Hollow Rocks
-1 Zombie Rope Climb`
+50m Run
+10 Alternating Lunges
+4 Up Downs
+4 Inchworms
+4 Roll and Reach
+4 Burpees
+4 Double Dumbbell Deadlifts
+4 Double Dumbbell Hang Snatch`
     },
     {
-      titulo: "Custom Metcon (Tiempo)",
-      contenido: `*Estadio Azteca*
+      titulo: "Custom Metcon",
+      contenido: `*Cannonball!*
+*Part 1*
 *Teams of 2*
-For time:
-300 Double Unders
-30 Rope Climbs / 120 Pull-ups
-300 Double Unders
-Time cap: 20 mins`
+Complete as many rounds as possible in 25 mins of:
+200 m Partner Run
+20 Devil Press, 22.5/15 - split
+Rest 5 mins before part 2...
+*Part 2*
+Power Clean 1x1`
     },
     {
       titulo: "Accesorio",
-      contenido: `*Mini Pump - Core*
-*3 rounds* for quality of:
+      contenido: `*4 rounds for quality of:*
+6 Deadlifts,* RPE 7/10*
+15 Weighted Hip Thrusts,* RPE 8/10*
+10 Jefferson Curls,* RPE 7/10*
 10 Strict Toes-to-bars
-40 Russian Twists- weighted;* RPE 9/10*
-L-Sit Hold, 20 secs
-Rest 1-1:30 min between rounds.`
+Rest 2 mins`
     }
   ]
 };
